39eb03b82fdfdee21b647ce502f7264a42c8e35f
Leonardo Vargas <leu1607@hotmail.com>
2022-02-05 17:30:35 -0300

    fix: corrigindo erro de background no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,12 @@ import LogoCodelandia from 'static/logo-codelandia.svg'
 import styles from './styles.module.scss'
 
 export const Footer = () => (
-  <footer className={styles.container}>
-    <h3>@ {new Date().getFullYear()} - Leo Vargas</h3>
-    <h3>
-      Powered by <LogoCodelandia />
-    </h3>
-  </footer>
+  <section className={styles.background}>
+    <footer className={styles.container}>
+      <h3>@ {new Date().getFullYear()} - Leo Vargas</h3>
+      <h3>
+        Powered by <LogoCodelandia />
+      </h3>
+    </footer>
+  </section>
 )
